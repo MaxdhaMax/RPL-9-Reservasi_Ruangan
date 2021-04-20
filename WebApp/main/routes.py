e7b3ac773c72b9f27f1be11974440e08e32911e4
M Dafa <78836795+dafaath@users.noreply.github.com>
2021-04-20 23:04:01 +0700

    Menambahkan homepage yang sudah diperbaiki serta mengganti error.html

--- WebApp/main/routes.py
+++ WebApp/main/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request
-from flask_login import login_required
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user
 from WebApp.model import Post
 
 main = Blueprint('main', __name__)
@@ -7,6 +7,8 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 def landing_page():
+    if(current_user.is_authenticated):
+        return redirect(url_for('main.home'))
     return render_template('landing.html')
 
 
@@ -16,7 +18,10 @@ def home():
     # page = request.args.get('page', 1, type=int)
     # posts = Post.query.order_by(
     #     Post.datePosted.desc()).paginate(page=page, per_page=5)
-    return render_template("homepage.html")
+    username = current_user.username
+    p_image = current_user.image_file
+    print(p_image)
+    return render_template("homepage.html", username=username, p_image=p_image)
 
 
 @main.route("/about")
