66ad3957312f9c469154e5ac5558a04cdb54bd81
M Dafa <78836795+dafaath@users.noreply.github.com>
2021-04-20 20:52:40 +0700

    memasukan homepage kedalam templates sehingga bisa digunakan untuk program

--- WebApp/main/routes.py
+++ WebApp/main/routes.py
@@ -13,10 +13,10 @@ def landing_page():
 @main.route("/home")
 @login_required
 def home():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(
-        Post.datePosted.desc()).paginate(page=page, per_page=5)
-    return render_template("index.html", posts=posts)
+    # page = request.args.get('page', 1, type=int)
+    # posts = Post.query.order_by(
+    #     Post.datePosted.desc()).paginate(page=page, per_page=5)
+    return render_template("homepage.html")
 
 
 @main.route("/about")
