35874c949a11a08e326a5b9a33bd4c9b3944d4f4
M Dafa <78836795+dafaath@users.noreply.github.com>
2021-04-26 22:56:18 +0700

    Menambahkan fitur search di homepage

--- WebApp/config.py
+++ WebApp/config.py
@@ -1,14 +1,18 @@
 import os
 from dotenv import load_dotenv, find_dotenv
+from tempfile import mkdtemp
 load_dotenv(find_dotenv())
 
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ELASTICSEARCH_URL = os.getenv('ELASTICSEARCH_URL')
+    ELASTIC_USERNAME = os.getenv('ELASTIC_USERNAME')
+    ELASTIC_PASSWORD = os.getenv('ELASTIC_PASSWORD')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv('EMAIL')
     MAIL_PASSWORD = os.getenv('EMAILPASSWORD')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
